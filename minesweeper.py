0d1c00376ab2d3837e0d59f455138b9853390116
Kshitij Wadkar <kshitijwadkar@hotmail.co.uk>
2024-07-24 22:27:33 +0100

    Finished all function implemntations, completed all tasks, and optimised as per style50

--- minesweeper.py
+++ minesweeper.py
@@ -2,6 +2,7 @@ import itertools
 import random
 from copy import deepcopy
 
+
 class Minesweeper():
     """
     Minesweeper game representation
@@ -201,22 +202,22 @@ class MinesweeperAI():
         # Adds all possible neighbouring cells to the set, given the input cell's location
         # (i.e. it is not possible for a cell in the top row to have a neighbour above it, etc.)
         # top left
-        if cell[0] > 0 and cell [1] > 0:
+        if cell[0] > 0 and cell[1] > 0:
             cellNeighbours.add((cell[0]-1, cell[1]-1))
         # top centre
         if cell[0] > 0:
             cellNeighbours.add((cell[0]-1, cell[1]))
         # top right
-        if cell[0] > 0 and cell [1] < self.width-1:
+        if cell[0] > 0 and cell[1] < self.width-1:
             cellNeighbours.add((cell[0]-1, cell[1]+1))
         # middle left
-        if cell [1] > 0:
+        if cell[1] > 0:
             cellNeighbours.add((cell[0], cell[1]-1))
         # middle right
         if cell[1] < self.width-1:
             cellNeighbours.add((cell[0], cell[1]+1))
         # bottom left
-        if cell[0] < self.height-1 and cell [1] > 0:
+        if cell[0] < self.height-1 and cell[1] > 0:
             cellNeighbours.add((cell[0]+1, cell[1]-1))
         # bottom centre
         if cell[0] < self.height-1:
@@ -225,7 +226,7 @@ class MinesweeperAI():
         if cell[0] < self.height-1 and cell[1] < self.width-1:
             cellNeighbours.add((cell[0]+1, cell[1]+1))
 
-        #Deepcopy made as changing a set within the loop causes an error
+        # Deepcopy made as changing a set within the loop causes an error
         neighboursCopy = deepcopy(cellNeighbours)
 
         # If a neighbour is already known to be a mine or safe, it is removed from the set and count is changed accordingly
@@ -262,7 +263,7 @@ class MinesweeperAI():
         # Will contain exactly one copy of the sentences which appear more than once in self.knowledge
         badList = []
         # Will contain exactly one copy of the sentences which appear only once in self.knowledge
-        goodList= []
+        goodList = []
 
         # For each sentence in self.knowledge
         for sentenceNum in range(len(self.knowledge)):
@@ -280,7 +281,6 @@ class MinesweeperAI():
         # Set self.knowledge equal to goodlist plus badlist (i.e. the list containing exactly one copy of each sentence)
         self.knowledge = deepcopy(goodList+badList)
 
-
         # Removes empty sentences
         for sentence in self.knowledge:
             if sentence.count == len(sentence.cells):
@@ -295,14 +295,40 @@ class MinesweeperAI():
     def blanket_set_subtraction(self):
         # Set subtraction
         originalLength = len(self.knowledge)
+        # Loops thorugh all sets in knowledge
         for sentenceNum in range(len(self.knowledge)):
             firstSet = self.knowledge[sentenceNum]
+            
+            # Loops through all sets between the current set and the end of knowledge
             for nestedSentenceNum in range(sentenceNum+1, originalLength):
                 secondSet = self.knowledge[nestedSentenceNum]
+                # If the first set is a subset of the second...
                 if firstSet.cells.issubset(secondSet.cells):
-                    self.knowledge.append(Sentence(secondSet.cells.difference(firstSet.cells), secondSet.count-firstSet.count))
+                    sentenceAlreadyAdded = False
+                    
+                    for sentence in self.knowledge:
+                        if sentence.cells == secondSet.cells.difference(firstSet.cells):
+                            sentenceAlreadyAdded = True
+                    
+                    # And the symmetric difference between the sets has not already been added to knowledge...
+                    if not sentenceAlreadyAdded:
+                        # Add the symmetric difference to knowledge
+                        self.knowledge.append(Sentence(secondSet.cells.difference(
+                            firstSet.cells), secondSet.count-firstSet.count))
+                
+                # Otherwise if the second set is a subset of the first...
                 elif secondSet.cells.issubset(firstSet.cells):
-                    self.knowledge.append(Sentence(firstSet.cells.difference(secondSet.cells), firstSet.count - secondSet.count))
+                    sentenceAlreadyAdded = False
+
+                    for sentence in self.knowledge:
+                        if sentence.cells == firstSet.cells.difference(secondSet.cells):
+                            sentenceAlreadyAdded = True
+                    
+                    # And the symmetric difference between the sets has not already been added to knowledge...
+                    if not sentenceAlreadyAdded:
+                        # Add the symmetric difference to knowledge
+                        self.knowledge.append(Sentence(firstSet.cells.difference(
+                            secondSet.cells), firstSet.count - secondSet.count))
 
     def add_knowledge(self, cell, count):
         """
@@ -327,15 +353,12 @@ class MinesweeperAI():
         # Marks the input cell as a move that has been made
         self.moves_made.add(cell)
 
-
         # Marks the input cell as a safe cell
         self.mark_safe(cell)
 
-
         # Contains a sentence regarding neighbours to the input cell, of which we are not sure of the status
-            # (i.e we don't know if the neighbours are mines or safes)
+        # (i.e we don't know if the neighbours are mines or safes)
         uncertainNeighbours = deepcopy(self.find_uncertain_neighbours(cell, count))
-
 
         # If there are neighbouring cells which we don't already know the state of
         # AND we don't know for sure based on count whether or not they are mines 
@@ -343,43 +366,14 @@ class MinesweeperAI():
             # Add a sentence based on this information to self.knowledge
             self.knowledge.append(uncertainNeighbours)
 
-            # # Now for set subtraction...
-            # for iterableSentence in cachedLocalKnowledge:
-            #     # If there exists a sentence in knowledge which contains a cell set which is a subset 
-            #     if iterableSentence.cells.issubset(uncertainNeighbours.cells):
-
-            #         newSet = uncertainNeighbours.cells.difference(iterableSentence.cells)
-            #         newCount = uncertainNeighbours.count - iterableSentence.count
-                    
-            #         if newCount == 0:
-            #             for safe in newSet:
-            #                 self.mark_safe(safe)
-            #         elif newCount == len(newSet):
-            #             for mine in newSet:
-            #                 self.mark_mine(mine)
-            #         else:
-            #             self.knowledge.append(Sentence(newSet, newCount))
-
-            #     if uncertainNeighbours.cells.issubset(iterableSentence.cells):
-            #         newSet = iterableSentence.cells.difference(uncertainNeighbours.cells)
-            #         newCount = iterableSentence.count - uncertainNeighbours.count
-
-            #         if newCount == 0:
-            #             for safe in newSet:
-            #                 self.mark_safe(safe)
-            #         elif newCount == len(newSet):
-            #             for mine in newSet:
-            #                 self.mark_mine(mine)
-            #         else:
-            #             self.knowledge.append(Sentence(newSet, newCount))
-
-
-        # Remove duplicates and empty sentences in self.knowledge
+        # Remove duplicates and empty sentences from self.knowledge
         self.knowledge_cleanup()
 
         # Runs set subtraction on all sets in self.knowledge
         self.blanket_set_subtraction()
 
+        # Runs again, this time not to check for duplicates or empty cells,
+        # but to check if subtraction resulted in any additional mines/safes (and mark them)
         self.knowledge_cleanup()
 
     def make_safe_move(self):
@@ -391,7 +385,11 @@ class MinesweeperAI():
         This function may use the knowledge in self.mines, self.safes
         and self.moves_made, but should not modify any of those values.
         """
-        raise NotImplementedError
+        
+        untouchedSafes = self.safes.difference(self.moves_made)
+        
+        for safe in untouchedSafes:
+            return safe
 
     def make_random_move(self):
         """
@@ -400,4 +398,8 @@ class MinesweeperAI():
             1) have not already been chosen, and
             2) are not known to be mines
         """
-        raise NotImplementedError
+        while True:
+            randomCoords = (random.randrange(self.height), random.randrange(self.width))
+
+            if randomCoords not in self.moves_made and randomCoords not in self.mines:
+                return randomCoords
